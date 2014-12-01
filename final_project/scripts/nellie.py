531bd1bd615c788c022e9fafcb497ec4baeb4134
Sharon <sharong@Sharons-computer.(none)>
2014-12-01 11:41:12 -0500

    merge

--- final_project/scripts/nellie.py
+++ final_project/scripts/nellie.py
@@ -7,17 +7,7 @@ import numpy as np
 import argparse
 import cv2
 import cv
-from std_msgs.msg import String
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import Vector3
-from sensor_msgs.msg import LaserScan
-import rospy
-import numpy as np
-import argparse
-import cv2
-import cv
+import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -36,10 +26,13 @@ class Nellie():
 
 		self.bridge = CvBridge()
 		self.cv_image = Image()
-		self.motion = True
+
 		self.cx = 0
 		self.turn=0
 		self.vel=0
+		self.obstacle=False
+		self.seeColor=False
+		
 		print "I made Nellie"
 
 	def audio(self, msg):
@@ -48,28 +41,67 @@ class Nellie():
 
 	def camera(self, msg):
 		#color recognition code
-		bridge = CvBridge()
-		cv_image = Image()
+		print "hi"
+		if self.obstacle is False:
+			print "looking for color"
+			try:
+				self.cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+			except CvBridgeError, e:
+				print e
 
-		cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
+			# using a BGR range to detect red
+			lowerR = np.array([2,0,85], "uint8")
+			upperR = np.array([65,71,215], "uint8")        
 
-		# using a BGR range to detect red
-		lowerR = np.array([2,0,85], "uint8")
-		upperR = np.array([65,71,215], "uint8")        
+			# using a BGR range to detect blue
+			lowerB = np.array([60, 16, 0], "uint8")
+			upperB = np.array([235, 103, 65], "uint8")
 
-		# using a BGR range to detect blue
-		lowerB = np.array([60, 16, 0], "uint8")
-		upperB = np.array([235, 103, 65], "uint8")
+			# using inRange to detect red stop signal
+			redStop = cv2.inRange(self.cv_image, lowerR, upperR)
+			redStopImage = cv2.bitwise_and(self.cv_image, self.cv_image, mask = redStop)
 
-		# using inRange to detect red thing
-		redThing = cv2.inRange(cv_image, lowerR, upperR)
-		redThingImage = cv2.bitwise_and(cv_image, cv_image, mask = redThing)
+			# using inRange to detect blue turn signal
+			blueTurn = cv2.inRange(self.cv_image, lowerB, upperB)
+			blueTurnImage = cv2.bitwise_and(self.cv_image, self.cv_image, mask = blueTurn)
 
-		# using inRange to detect blue things
-		blueThing = cv2.inRange(cv_image, lowerB, upperB)
-		blueThingImage = cv2.bitwise_and(cv_image, cv_image, mask = blueThing)
+			# finding contours for red and blue blobs
+			contoursR, hierarchyR = cv2.findContours(redStop, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+			contoursB, hierarchyB = cv2.findContours(blueTurn, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+			
+			# finding contour with maximum area and store it as best_cnt
+			max_area = 0
+			best_cnt = None
 
-		cv2.imshow('Original', np.hstack([cv_image, redThingImage]))
+			# is the biggest blob red?
+			for cntR in contoursR:
+				areaR = cv2.contourArea(cntR)
+
+				if areaR > max_area:
+					max_area = areaR
+					best_cnt = cntR
+					signal_img = redStopImage
+					print "red"
+					self.turn=0
+					self.vel=.2
+					self.seeColor=True
+					#time.sleep(5)
+					msg=Twist(Vector3(self.vel,0.0,0.0),Vector3(0.0,0.0,self.turn))
+					self.pub.publish(msg)
+
+			# or is the biggest blob blue?
+			for cntB in contoursB:
+				areaB = cv2.contourArea(cntB)
+				if areaB > max_area:
+					max_area = areaB
+					best_cnt = cntB
+					signal_img = blueTurnImage
+					print "blue"
+					self.turn=.5
+					self.vel=0
+					self.seeColor=True
+					msg=Twist(Vector3(self.vel,0.0,0.0),Vector3(0.0,0.0,self.turn))
+					self.pub.publish(msg)
 
 	def laser(self, msg):
 		valid_ranges = []
@@ -89,20 +121,21 @@ class Nellie():
 					print "Obstacle!"
 					self.turn=-.5
 					self.vel=0
+					self.obstacle=True
 					msg=Twist(Vector3(self.vel,0.0,0.0),Vector3(0.0,0.0,self.turn))
 					self.pub.publish(msg)
 				else: #Stop and check for color/voice command
 					print "No obstacle"
 					self.turn=0
-					self.vel=.1
+					self.vel=.2
+					self.obstacle=False
 					msg=Twist(Vector3(self.vel,0.0,0.0),Vector3(0.0,0.0,self.turn))
 					self.pub.publish(msg)
-					#color(self,msg)
 		else:
 			"I see nothing"
 			self.vel=0
 			self.turn=0
-			#color(self,msg)
+			self.obstacle=False
 
 	def run(self):
 		r = rospy.Rate(10) # 10hz
